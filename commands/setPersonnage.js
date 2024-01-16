d8301badd096e01917f2ae6506b3e06b2938e5c4
0ruj <ccommunay@gmail.com>
2024-01-16 01:21:23 +0100

    - add logic set new stats perso

---
+++ commands/setPersonnage.js
@@ -0,0 +1,45 @@
+const { ApplicationCommandOptionType } = require('discord-api-types/v9');
+const Personnage = require('../models/personnage');
+const { BOOLEAN } = require('sequelize');
+
+module.exports = {
+    name: 'set',
+    description: 'Enregistre un nouveau personnage avec ses stats',
+    options: [{
+        type: ApplicationCommandOptionType.String,
+        name: 'stats',
+        description: 'Stats du personnage sous forme personnage/vie/endurance/attaque/defense/vitesse',
+        required: true,
+    }],
+    async execute(interaction) {
+        const stats = interaction.options.getString('stats').split('/');
+        if (stats.length !== 6) {
+            return await interaction.reply("Allume ton cerveau, je t'ai dit le format de saisie. Utilise exactement ça: personnage/vie/endurance/attaque/defense/vitesse");
+        }
+
+        let [nom, vie, endurance, attaque, defense, vitesse] = stats;
+        nom = nom.charAt(0).toUpperCase() + nom.slice(1).toLowerCase();
+
+        // Vérifier si le personnage existe déjà
+        const personnageExistant = await Personnage.findOne({ where: { nom: nom } });
+        if (personnageExistant) {
+            return await interaction.reply(`${nom} existe déjà dans la base de données. Tu dors au fond de la classe à côté du radiateur ?`);
+        }
+
+        try {
+            await Personnage.create({
+                nom,
+                vie: parseInt(vie),
+                endurance: parseInt(endurance),
+                attaque: parseInt(attaque),
+                defense: parseInt(defense),
+                vitesse: parseInt(vitesse)
+            });
+
+            await interaction.reply(`${nom} sauvegardé(e) avec succès !`);
+        } catch (error) {
+            console.error(error);
+            await interaction.reply('Il y a eu une erreur lors de la création du personnage.');
+        }
+    }
+};
